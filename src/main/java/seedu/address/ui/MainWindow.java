2c37039c7f1122c2fc016920ac6e4e131207e63f
Julian <Julianlimwq12@Gmail.com>
2026-03-12 22:20:38 +0800

    Reorder hostServices to conform to checkstyle

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -2,8 +2,8 @@ package seedu.address.ui;
 
 import java.util.logging.Logger;
 
-import javafx.event.ActionEvent;
 import javafx.application.HostServices;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextInputControl;
