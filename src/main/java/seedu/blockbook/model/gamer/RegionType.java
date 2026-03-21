cd20e4bde08d105397a24f024b4b62340b10b9fc
Teh Hock Jian <hockjianteh@gmail.com>
2026-03-21 21:04:00 +0800

    Create RegionType.java for enum values

---
+++ src/main/java/seedu/blockbook/model/gamer/RegionType.java
@@ -0,0 +1,16 @@
+package seedu.blockbook.model.gamer;
+
+/**
+ * Supported region codes:
+ * NA (North America), SA (South America), EU (Europe),
+ * AFRICA, ASIA, OCEANIA, and ME (Middle East).
+ */
+public enum RegionType {
+    NA,
+    SA,
+    EU,
+    AFRICA,
+    ASIA,
+    OCEANIA,
+    ME
+}
